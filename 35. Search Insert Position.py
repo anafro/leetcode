11d7286c3759b65585a3286f37eb4c6f67c4bcdb
Anatoly <contact@anafro.ru>
2024-11-11 21:57:01 +1000

    feat: 35. Search Insert Position

---
+++ 35. Search Insert Position.py
@@ -0,0 +1,34 @@
+﻿# ----------------------------
+# Category: Easy
+# Leetcode link: https://leetcode.com/problems/search-insert-position
+# ----------------------------
+from typing import List
+
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            middle = (left + right) // 2
+            element = nums[middle]
+
+            if element < target:
+                left = middle + 1
+
+            if element > target:
+                right = middle - 1
+
+            if element == target:
+                index = middle
+
+                while nums[index] == target:
+                    if index == 0:
+                        return 0
+
+                    index -= 1
+
+                return index + 1
+
+        return (left + right) // 2 + 1
