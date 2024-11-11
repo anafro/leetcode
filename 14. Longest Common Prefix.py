1f8d93b3459dc0144cdaa05756fb4cc761270900
Anatoly <contact@anafro.ru>
2024-11-11 20:00:08 +1000

    feat: 14. Longest Common Prefix

---
+++ 14. Longest Common Prefix.py
@@ -0,0 +1,19 @@
+﻿# ----------------------------
+# Category: Easy
+# Leetcode link: https://leetcode.com/problems/longest-common-prefix/
+# ----------------------------
+from typing import List
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        min_length = min(len(str) for str in strs)
+
+        for prefix_length in range(min_length + 1):
+            prefix = strs[0][:prefix_length]
+
+            for string in strs:
+                if not string.startswith(prefix):
+                    return prefix[:-1]
+
+        return min(strs, key=len)
