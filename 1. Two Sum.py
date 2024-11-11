7f26b767da95c4e12b169cac573a4f93b06ff441
Anatoly <contact@anafro.ru>
2024-11-11 14:55:10 +1000

    feat: 1. Two Sum

---
+++ 1. Two Sum.py
@@ -0,0 +1,30 @@
+﻿# ----------------------------
+# Category: Easy
+# Leetcode link: https://leetcode.com/problems/two-sum/
+# ----------------------------
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        sorted_nums = nums.copy()
+        sorted_nums.sort()
+
+        left = 0
+        right = len(nums) - 1
+
+        while left < right:
+            left_num = sorted_nums[left]
+            right_num = sorted_nums[right]
+            sum = left_num + right_num
+
+            if sum > target:
+                right -= 1
+
+            if sum < target:
+                left += 1
+
+            if sum == target:
+                return [nums.index(left_num), len(nums) - nums[::-1].index(right_num) - 1]
+
+        return []
