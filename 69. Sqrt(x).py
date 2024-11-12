8e08cfd8eebb3e9b5e6986eaca50342cd197838d
Anatoly <contact@anafro.ru>
2024-11-12 13:43:46 +1000

    feat: 69. Sqrt(x)

---
+++ 69. Sqrt(x).py
@@ -0,0 +1,33 @@
+﻿# ----------------------------
+# Category: Easy
+# Leetcode link: https://leetcode.com/problems/sqrtx/
+# ----------------------------
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        left = 0
+        right = x
+        middle = 0
+
+        while left <= right:
+            middle = (left + right) // 2
+            square = middle ** 2
+
+            if square == x:
+                return middle
+
+            if square > x:
+                right = middle - 1
+
+            if square < x:
+                if (middle + 1) ** 2 > x:
+                    return middle
+
+                if (middle + 1) ** 2 == x:
+                    return middle + 1
+
+                left = middle + 1
+
+        return middle
+
+print(Solution().mySqrt(1))
