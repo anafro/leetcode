77428d7e04f181dcd9140a1e23e91324e4e0fbd3
Anatoly <contact@anafro.ru>
2024-11-12 16:44:33 +1000

    feat: 100. Same Tree

---
+++ 100. Same Tree.py
@@ -0,0 +1,23 @@
+﻿# ----------------------------
+# Category: Easy
+# Leetcode link: https://leetcode.com/problems/same-tree/
+# ----------------------------
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        def propagate_comparison(node_a: Optional[TreeNode], node_b: Optional[TreeNode]) -> bool:
+            if node_a is None or node_b is None:
+                return node_a is node_b
+
+            return node_a.val == node_b.val and propagate_comparison(node_a.left, node_b.left) and propagate_comparison(node_a.right, node_b.right)
+
+        return propagate_comparison(p, q)
