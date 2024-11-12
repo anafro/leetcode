7ace977a292ad151c3fc658a63bb147132978c12
Anatoly <contact@anafro.ru>
2024-11-13 00:11:22 +1000

    feat: 2864. Maximum Odd Binary Number

---
+++ 2864. Maximum Odd Binary Number.py
@@ -0,0 +1,28 @@
+﻿# ----------------------------
+# Category: Easy
+# Leetcode link: https://leetcode.com/problems/maximum-odd-binary-number/
+# ----------------------------
+class Solution:
+    def maximumOddBinaryNumber(self, s: str) -> str:
+        ones = 0
+        zeros = 0
+        result = []
+
+        for char in s:
+            if char == '1':
+                ones += 1
+
+            if char == '0':
+                zeros += 1
+
+        while ones > 1:
+            result.append('1')
+            ones -= 1
+
+        while zeros > 0:
+            result.append('0')
+            zeros -= 1
+
+        result.append('1')
+
+        return ''.join(result)
