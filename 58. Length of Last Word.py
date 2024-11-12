3c48365ea670171d925c3ccbd8542f8ad71364a7
Anatoly <contact@anafro.ru>
2024-11-12 13:05:58 +1000

    feat: 58. Length of Last Word

---
+++ 58. Length of Last Word.py
@@ -0,0 +1,22 @@
+﻿# ----------------------------
+# Category: Easy
+# Leetcode link: https://leetcode.com/problems/length-of-last-word/
+# ----------------------------
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        index = len(s) - 1
+        word_length = 0
+        
+        while index >= 0:
+            character = s[index]
+
+            if character != ' ':
+                word_length += 1
+
+            if character == ' ' and word_length != 0:
+                return word_length
+
+            index -= 1
+
+
+        return word_length
