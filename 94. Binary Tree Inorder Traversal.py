347b1a7ddde2990f96b2565df7e84fd6dd998382
Anatoly <contact@anafro.ru>
2024-11-12 16:03:04 +1000

    feat: 94. Binary Tree Inorder Traversal

---
+++ 94. Binary Tree Inorder Traversal.py
@@ -0,0 +1,28 @@
+﻿# ----------------------------
+# Category: Easy
+# Leetcode link: https://leetcode.com/problems/binary-tree-inorder-traversal
+# ----------------------------
+from typing import Optional, List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        traversal_items = []
+
+        def propagate_traversal(node: Optional[TreeNode]) -> None:
+            if node is None:
+                return
+
+            propagate_traversal(node.left)
+            traversal_items.append(node.val)
+            propagate_traversal(node.right)
+
+        propagate_traversal(root)
+        return traversal_items
